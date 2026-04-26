e67c4ec6eb439250e38c0a93e0e42a760ee2ca35
Anthony Lee <hososlch@gmail.com>
2026-04-26 19:50:36 +0800

    refactor: simplify AppController by removing quotes endpoint and integrating StockPricesController

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,11 @@
 import type {Response} from "express";
 import {encode} from "@toon-format/toon";
-import {StockPricesService} from "./stock-prices/stock-prices.service";
-import {BadRequestException, Controller, Get, Query, Res} from "@nestjs/common";
+import {Controller, Get, Res} from "@nestjs/common";
 
 @Controller()
 export class AppController {
-    constructor(private readonly stockPricesService: StockPricesService) {}
-
     @Get()
     getRoot(@Res() res: Response): void {
         res.send(encode({message: "Stock Prices API", version: "1.0.0"}));
-    }
-
-    @Get("quotes")
-    async getQuotes(@Query("symbols") symbols: string | undefined, @Res() res: Response): Promise<void> {
-        if (!symbols) {
-            throw new BadRequestException({error: "symbols is required"});
-        }
-
-        try {
-            const symbolsArray = symbols.split(",").map(symbol => symbol.trim());
-            const quotes = await this.stockPricesService.getQuotes(symbolsArray);
-
-            res.send(encode({quotes}));
-        } catch (error) {
-            console.error("Error fetching quotes:", error);
-        }
     }
 }
--- src/stock-prices/stock-prices.module.ts
+++ src/stock-prices/stock-prices.module.ts
@@ -1,7 +1,9 @@
 import {Module} from "@nestjs/common";
+import {StockPricesController} from "./stock-prices.controller";
 import {StockPricesService} from "./stock-prices.service";
 
 @Module({
+    controllers: [StockPricesController],
     providers: [StockPricesService],
     exports: [StockPricesService],
 })
