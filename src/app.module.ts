5f034919b477fd3ce032ea7b49c223e22e373463
Anthony Lee <hososlch@gmail.com>
2026-09-07 14:45:35 +0800

    refactor: from nodejs to rust

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module} from "@nestjs/common";
-import {RootController} from "./app.controller";
-import {StockPricesModule} from "./stock-prices/stock-prices.module";
-
-@Module({
-    imports: [StockPricesModule],
-    controllers: [RootController],
-})
-export class AppModule {}
--- src/stock-prices/stock-prices.module.ts
+++
@@ -1,9 +0,0 @@
-import {Module} from "@nestjs/common";
-import {StockPricesService} from "./stock-prices.service";
-import {StockPricesController} from "./stock-prices.controller";
-
-@Module({
-    controllers: [StockPricesController],
-    providers: [StockPricesService],
-})
-export class StockPricesModule {}
